92ba46e0179c41b4034a42774984095918d8dc95
minkyojeong <redwonder@naver.com>
2019-06-09 16:24:55 +0900

    2019.06.09 : 1520 문제 해결
    기존 BFS로 문제를 풀경우, 모든 경우를 다 돌게 되어서 시간초과가 뜸.
    해당 문제를 DP로 문제해결

--- src/problem_1520/Main.java
+++ src/problem_1520/Main.java
@@ -1,62 +1,45 @@
 package problem_1520;
 
-import java.util.ArrayList;
 import java.util.Scanner;
-
-class Node {
-    int left;
-    int right;
-    int value;
-
-    public Node(int left, int right, int value){
-        this.left = left;
-        this.right = right;
-        this.value = value;
-    }
-}
 
 public class Main {
 
     static int M,N;
-    static int final_cnt = 0;
-    static Node [][] table = new Node[501][501];
-    static ArrayList<Node> q = new ArrayList<Node>();
+    static int [][] table = new int[501][501];
+    static int [][] dp = new int[501][501];
 
-    public static void DownHill(int i, int j){
+    public static int DownHill(int i, int j){
+
+        if(i == 0 && j == 0) return 1;
+        if(dp[i][j] != -1) return dp[i][j];
+
+        dp[i][j] = 0;
+
         if (i-1 >= 0) {
-            if(table[i-1][j].value < table[i][j].value){
-                q.add(table[i-1][j]);
+            if(table[i-1][j] > table[i][j]){
+                dp[i][j] += DownHill(i-1,j);
             }
         }
 
         if (i+1 < M) {
-            if(table[i+1][j].value < table[i][j].value){
-                q.add(table[i+1][j]);
+            if(table[i+1][j] > table[i][j]){
+                dp[i][j] += DownHill(i+1,j);
             }
         }
         if (j-1 >= 0) {
-            if(table[i][j-1].value < table[i][j].value){
-                q.add(table[i][j-1]);
+            if(table[i][j-1] > table[i][j]){
+                dp[i][j] += DownHill(i,j-1);
             }
         }
         if (j+1 < N) {
-            if(table[i][j+1].value < table[i][j].value){
-                q.add(table[i][j+1]);
+            if(table[i][j+1] > table[i][j]){
+                dp[i][j] += DownHill(i,j+1);
             }
         }
+
+        return dp[i][j];
     }
 
-    static void CycleQueue(){
-        while(q.size() != 0){
-            Node node = q.get(0);
-            q.remove(0);
-            int x = node.left;
-            int y = node.right;
-            if(x == M-1 && y == N-1)
-                final_cnt += 1;
-            DownHill(x,y);
-        }
-    }
 
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
@@ -65,14 +48,13 @@ public class Main {
 
         for(int i = 0; i < M; i++){
             for(int j = 0; j < N; j++){
-                Node node = new Node(i, j, scan.nextInt());
-                table[i][j] = node;
+                table[i][j] = scan.nextInt();
+                dp[i][j] = -1;
             }
         }
 
-        DownHill(0,0);
-        CycleQueue();
+        DownHill(M-1,N-1);
 
-        System.out.println(final_cnt);
+        System.out.println(dp[M-1][N-1]);
     }
 }
